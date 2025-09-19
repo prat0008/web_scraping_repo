5c4f58d3d5b21e983d87bd6df39b06e493ccaf5e
prat0008 <prateekgoel142@gmail.com>
2025-09-19 14:31:48 +0530

    updated

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # web_scraping_repo
+
 web scraping practice
+<br>
+tata ipl auction stats
 <br>
 Made by Prateek Goel
--- web_scraping.py
+++ web_scraping.py
@@ -1,100 +1,32 @@
-#Explanation:GET request is sent to the URL using requests library.
-#            .text attribute of the response object returns HTML content of the page as a string.
-
-"""import requests
-url = "https://www.geeksforgeeks.org/dsa/dsa-tutorial-learn-data-structures-and-algorithms/"
-response = requests.get(url)
-print(response.text)
-print(response.status_code)"""
-
-"""import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
-r = requests.get(url)
-soup = BeautifulSoup(r.text,"lxml")
-
-names = soup.find_all("a",class_ = "title")
-product_list = []
-for i in names:
-    name = i.text.strip()
-    product_list.append(name)
-#print(product_list)
-
-prices = soup.find_all("h4",class_ = "price float-end card-title pull-right")
-prices_list = []
-for i in prices:
-    price = i.text.strip()
-    prices_list.append(price)
-#print(prices_list)
-
-description = soup.find_all("p",class_="description card-text")
-desc_list = []
-for i in description:
-    desc = i.text.strip()
-    desc_list.append(desc)
-#print(desc_list)
-
-
-reviews = soup.find_all("p",class_="review-count float-end")
-reviews_list = []
-for i in reviews:
-    revw = i.text.strip()
-    reviews_list.append(revw)
-#print(reviews_list)
-
-df = pd.DataFrame({"Product_Name":product_list,"Prices":prices_list,"Description":desc_list,"Reviews":reviews_list})
-print(df)
-
-df.to_csv("Product_Details.csv")"""
-
-
-#import requests
-#from bs4 import BeautifulSoup
-#url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
-#r = requests.get(url)
-#soup = BeautifulSoup(r.text,"lxml")
-
-#boxes = soup.find_all("div",class_="col-md-4 col-xl-4 col-lg-4")
-#print(boxes)
-#print(len(boxes))
-
-#box=soup.find_all("div",class_="col-md-4 col-xl-4 col-lg-4")[3]
-#print(box)
-
-#name=box.find("a",class_="title").text.strip()
-#print(name)
-#description=box.find("p",class_="description card-text").text.strip()
-#print(description)
-
-#nav_bar=soup.find("li",class_="nav-item")
-#side_menu=soup.find("li",class_="nav-item active")
-#print(side_menu.text)
-
+#project - tata ipl auction stats 
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url="https://ticker.finology.in/"
-r=requests.get(url)
-#print(r)
-soup=BeautifulSoup(r.text,"lxml")
-table=soup.find("table",class_="table table-sm table-hover screenertable")
-#print(table)
+url = "https://www.iplt20.com/auction/2022"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "lxml")
 
-titles=[]
-headers=table.find_all("th")
-for i in headers:
-    title=i.text
-    titles.append(title)
-#print(titles)
-df=pd.DataFrame(columns=titles)
-#print(df)
-rows=table.find_all("tr")
-for i in rows[1:]:
+# Find the specific table first
+table = soup.find("table", class_="ih-td-tab w-100 auction-tbl")
+
+# Then find headers ONLY within that table
+if table:
+    headers = table.find_all("th")
+    title_list = [header.text.strip() for header in headers]
+    #print(title_list)
+else:
+    pass
+    #print("Table not found")
+
+df=pd.DataFrame(columns=title_list)
+
+points_data=table.find_all("tr")
+for i in points_data[1:]:
     data=i.find_all("td")
-    row=[tr.text.strip() for tr in data]
+    points_data=[tr.text.strip() for tr in data]
     l=len(df)
-    df.loc[l]=row
+    df.loc[l]=points_data
 print(df)
+df.to_csv("Tata_IPL-Auction_Stats.csv")
