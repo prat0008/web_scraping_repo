2464fe2b0fdf9e4e695f94de1c1ed61f9e670e50
prat0008 <prateekgoel142@gmail.com>
2025-09-19 14:34:44 +0530

    upadted

--- web_scraping.py
+++ web_scraping.py
@@ -8,17 +8,12 @@ url = "https://www.iplt20.com/auction/2022"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "lxml")
 
-# Find the specific table first
 table = soup.find("table", class_="ih-td-tab w-100 auction-tbl")
-
-# Then find headers ONLY within that table
 if table:
     headers = table.find_all("th")
     title_list = [header.text.strip() for header in headers]
-    #print(title_list)
 else:
     pass
-    #print("Table not found")
 
 df=pd.DataFrame(columns=title_list)
 
